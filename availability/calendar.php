ba0ba026c47d0e2364fe80171c1363911ddfe303
Daniel Beardsley <daniel.beardsley@gmail.com>
2010-07-21 12:12:43 -0700

    Added availability calendar, with moderate security

---
+++ availability/calendar.php
@@ -0,0 +1,18 @@
+<? if (sha1( $_GET["group"]) != "93df14617ceda8ee499003ae137b44e91aa26cce") {
+	header("HTTP/1.1 404 Not Found");
+	flush();
+	exit();
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<title>Availability of Daniel Beardsley</title>
+	</head>
+	<body>
+		<iframe src="https://www.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=7b1q30lp77ojnf4hoqjnbh26mg%40group.calendar.google.com&amp;color=%23060D5E&amp;ctz=America%2FLos_Angeles" style=" border-width:0 " width="800" height="600" frameborder="0" scrolling="no"></iframe>
+	</body>
+</html>
